6a5f72a20c3c4774cbadf19fdf6e2f3ac97b36a5
LIANGJINGWU1 <1174713142ljw@gmail.com>
2025-05-14 20:28:53 +0800

    修改

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -1,21 +1,21 @@
 <?php
-namespace App\Providers;
-
-use App\Models\User;
-use App\Policies\UserPolicy;
-//use Illuminate\Support\ServiceProvider;
-use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider; // ✅ 正确的基类
-
-
-class AuthServiceProvider extends ServiceProvider{
-    protected $policies = [
-        User::class => UserPolicy::class,
-    ];
-
-    public function boot()
-    {
-        $this->registerPolicies();
-    }
-}
+//namespace App\Providers;
+//
+//use App\Models\User;
+//use App\Policies\UserPolicy;
+////use Illuminate\Support\ServiceProvider;
+//use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider; // ✅ 正确的基类
+//
+//
+//class AuthServiceProvider extends ServiceProvider{
+//    protected $policies = [
+//        User::class => UserPolicy::class,
+//    ];
+//
+//    public function boot()
+//    {
+//        $this->registerPolicies();
+//    }
+//}
 
 
