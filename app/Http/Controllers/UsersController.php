9e84d33a1d8f3e909a9dfa0f5bb08a2b3b89a3f1
LIANGJINGWU1 <1174713142ljw@gmail.com>
2025-05-14 17:40:04 +0800

    完成用户编辑功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -37,5 +37,26 @@ class UsersController extends Controller
         auth()->login($user);
         return redirect()->route('users.show', ['user' => $user])->with('success', "success");
     }
+    public function edit(User $user): Factory|Application|View
+    {
+        return view('users.edit', ['user' => $user]);
+    }
+
+    public function update(Request $request, User $user){
+        $request->validate([
+            'name' => 'required|max:50',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        $data = $request->only(['name']);
+        if($request->filled('password')){
+            $data = $request->only(['name', 'password']);
+        }
+        $user->update($data);
+
+        return redirect()->route('users.show', ['user' => $user])->with('success', "success");
+    }
+
+
 
 }
