7aabb3c91b5bb06e0379805bb2f145824df0232f
LIANGJINGWU1 <1174713142ljw@gmail.com>
2025-05-20 16:27:33 +0800

    创建粉丝数据列表

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -6,6 +6,26 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * 
+ *
+ * @property int $id
+ * @property string $content The content of the status
+ * @property int $user_id The user id of the status
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Database\Factories\StatusFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Status newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Status newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Status query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Status whereContent($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Status whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Status whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Status whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Status whereUserId($value)
+ * @property-read \App\Models\User|null $user
+ * @mixin \Eloquent
+ */
 class Status extends Model
 {
     use HasFactory;
@@ -14,7 +34,7 @@ class Status extends Model
         'content',
     ];
 
-    protected function user(): BelongsTo
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\DatabaseNotification;
@@ -14,7 +15,7 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Carbon;
 
 /**
- *
+ * 
  *
  * @property int $id
  * @property string $name
@@ -40,6 +41,18 @@ use Illuminate\Support\Carbon;
  * @method static Builder<static>|User wherePassword($value)
  * @method static Builder<static>|User whereRememberToken($value)
  * @method static Builder<static>|User whereUpdatedAt($value)
+ * @property int $activated
+ * @property string|null $activation_token
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Status> $feed
+ * @property-read int|null $feed_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, User> $followers
+ * @property-read int|null $followers_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, User> $following
+ * @property-read int|null $following_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Status> $statuses
+ * @property-read int|null $statuses_count
+ * @method static Builder<static>|User whereActivated($value)
+ * @method static Builder<static>|User whereActivationToken($value)
  * @mixin \Eloquent
  */
 class User extends Authenticatable
@@ -109,4 +122,49 @@ class User extends Authenticatable
     {
         return  $this->statuses()->orderBy('created_at', 'desc');
     }
+
+    /**
+     * @return BelongsToMany
+     */
+    public function followers(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
+    }
+
+    public function following(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'followers', 'follower_id','user_id');
+    }
+
+    /**
+     * 关注用户
+     */
+    public function follow($userIds): void
+    {
+        if(is_array($userIds)) {
+            $userIds = compact('userIds');
+        }
+        //使用sync方法前要有定义好的关系，只会添加新的记录，不删除旧记录.
+        $this->following()->sync($userIds, false);
+    }
+
+    /**
+     * 取关
+     * @param $userIds
+     * @return void
+     */
+    public function unfollow($userIds): void
+    {
+        if(is_array($userIds)) {
+            $userIds = compact('userIds');
+        }
+        //detach实现定义关系，删除指定记录
+        $this->following()->detach($userIds);
+    }
+
+    public function isFollowing(User $user): bool
+    {
+        return $this->followings->contains($user->id);
+    }
+
 }
